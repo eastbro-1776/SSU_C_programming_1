d595f0ed7ba27f91f3bb0889fc9f2d92cda56db7
eastbro-1776 <68009525+eastbro-1776@users.noreply.github.com>
2021-03-18 19:00:08 +0900

    Week 6

--- W6/W6_7.c
+++ W6/W6_7.c
@@ -5,10 +5,10 @@
 int main(void)
 {
     int a, b = 0, c = 0;
-	a = ++b + ++c;
+    a = ++b + ++c;
     printf("%d %d %d\n", a, b, c);
     a = b++ + c++;
-	printf("%d %d %d\n", a, b, c);
+    printf("%d %d %d\n", a, b, c);
     a = ++b + c++;
     printf("%d %d %d\n", a, b, c);
     a = b-- + --c;
